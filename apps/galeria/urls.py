bf4c9339b53a772fb55e391d95faf269edbff5f7
harrison <harrisonpinchieri@gmail.com>
2023-09-02 18:33:46 -0300

    v0.5

--- apps/galeria/urls.py
+++ apps/galeria/urls.py
@@ -6,6 +6,7 @@ from apps.galeria.views import (
     index,
     imagem,
     nova_imagem,
+    filtro,
 )
 
 urlpatterns = [
@@ -15,6 +16,7 @@ urlpatterns = [
     ),  # seleciona imagem dinamicamente
     path("buscar", buscar, name="buscar"),
     path("nova-imagem", nova_imagem, name="nova_imagem"),
-    path("editar-imagem", editar_imagem, name="editar_imagem"),
-    path("deletar-imagem", deletar_imagem, name="deletar_imagem"),
+    path("editar-imagem/<int:foto_id>", editar_imagem, name="editar_imagem"),
+    path("deletar-imagem/<int:foto_id>", deletar_imagem, name="deletar_imagem"),
+    path("filtro/<str:categoria>", filtro, name="filtro"),
 ]
--- apps/galeria/views.py
+++ apps/galeria/views.py
@@ -35,7 +35,7 @@ def buscar(request):
         if nome_a_buscar:
             fotografias = fotografias.filter(nome__icontains=nome_a_buscar)
 
-    return render(request, "galeria/buscar.html", {"cards": fotografias})
+    return render(request, "galeria/index.html", {"cards": fotografias})
 
 
 def nova_imagem(request):
@@ -46,7 +46,7 @@ def nova_imagem(request):
     form = FotografiaForms
 
     if request.method == "POST":
-        form = FotografiaForms(request.POST)
+        form = FotografiaForms(request.POST, request.FILES)
         if form.is_valid():
             form.save()
             messages.success(request, "Nova Fotografia cadastrada!")
@@ -55,9 +55,30 @@ def nova_imagem(request):
     return render(request, "galeria/nova_imagem.html", {"form": form})
 
 
-def editar_imagem(request):
-    pass
+def editar_imagem(request, foto_id):
+    fotografia = Fotografia.objects.get(id=foto_id)
+    form = FotografiaForms(instance=fotografia)
+
+    if request.method == "POST":
+        form = FotografiaForms(request.POST, request.FILES, instance=fotografia)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Fotografia editada com sucesso")
+            return redirect("index")
+
+    return render(
+        request, "galeria/editar_imagem.html", {"form": form, "foto_id": foto_id}
+    )
 
 
-def deletar_imagem(request):
-    pass
+def deletar_imagem(request, foto_id):
+    fotografia = Fotografia.objects.get(id=foto_id)
+    fotografia.delete()
+    messages.success(request, "deleção feita com sucesso!")
+    return redirect("index")
+
+
+def filtro(request, categoria):
+    fotografias = Fotografia.objects.filter(publicada=True, categoria=categoria)
+
+    return render(request, "galeria/index.html", {"cards": fotografias})
